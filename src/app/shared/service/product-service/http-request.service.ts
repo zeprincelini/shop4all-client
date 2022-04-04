ad9c8a342b37b5acd54ee7d8bc2bee1e4bdc9fec
zeprincelini <princelini4@gmail.com>
2022-04-05 00:57:57 +0100

    modified product page

--- src/app/shared/service/product-service/http-request.service.ts
+++ src/app/shared/service/product-service/http-request.service.ts
@@ -7,7 +7,6 @@ import { Injectable } from "@angular/core";
 export class HttpRequestService {
   private addProduct = "http://localhost:3000/api/v1/product";
   private products = "https://fakestoreapi.com/products";
-  private product = "https://fakestoreapi.com/product";
 
   constructor(private http: HttpClient) {}
 
@@ -20,6 +19,6 @@ export class HttpRequestService {
   }
 
   getProduct(id: string) {
-    return this.http.get(`${this.product}/${id}`);
+    return this.http.get(`${this.products}/${id}`);
   }
 }
